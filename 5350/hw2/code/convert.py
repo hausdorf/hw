e3f30f54d99f78f2c37c0eac62f55374607ae5bb
Alex Clemmer <clemmer.alexander@gmail.com>
2011-10-08 22:11:28 -0600

    Fix convert.py

--- 5350/hw2/code/convert.py
+++ 5350/hw2/code/convert.py
@@ -7,21 +7,23 @@ TEST = 'hw1-test'
 if __name__ == '__main__':
     curr = TEST
     f = open(curr)
-    g = open(curr + '-proc', 'w')
+    g = open(curr + '-p', 'w')
 
     for line in f.readlines():
         counter = 0
 
         for feature in line.strip().split():
-            if counter == 0 or float(feature) == 0:
-                print '%s' % feature,
+            if counter == 0:
+                #print '%s' % feature,
                 g.write('%s' % feature)
             else:
-                print '\t%s:%s' % (str(counter), feature),
+                if float(feature) == 0:
+                    continue
+                #print '\t%s:%s' % (str(counter), feature),
                 g.write('\t%s:%s' % (str(counter), feature))
 
             counter += 1
-        print
+        #print
         g.write('\n')
 
     f.close()
