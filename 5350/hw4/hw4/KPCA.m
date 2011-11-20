43f2e8a9c97289a4dc236305bc2ffd78fcc8dee3
Alex Clemmer <clemmer.alexander@gmail.com>
2011-11-19 18:35:13 -0700

    Add working KPCA code

--- 5350/hw4/hw4/KPCA.m
+++ 5350/hw4/hw4/KPCA.m
@@ -11,6 +11,8 @@ if d > D,
   error('Kernel PCA: you are trying to *increase* the dimension!');
 end;
 
+%X = X';
+
 % compute the NxN kernel matrix K from the data X
 % we will use an RBF kernel here having a bandwidth 0.1
 % RBF kernel for two examples xi and xj is defined as 
@@ -18,22 +20,23 @@ end;
 % The (i,j)-th entry K(i,j) of the kernel matrix denotes the similarity 
 % between example i and example j
 %TODO
+sig = median(pdist(X));
 K = zeros(N,N);
 for i=1:N
     for j=1:N
-        K(i,j) = exp(norm(X(i,:) - X(j,:), 2));
+        K(i,j) = exp(-(norm(X(i,:) - X(j,:), 2))^2 / (2*sig^2));
     end
 end
 
 % now center the kernel matrix K that you computed above
 % (see the slides from the class)
 %TODO
-ONE = zeros(N,N);
-for i=1:N
-    for j=1:N
-        ONE(i,j) = 1.0 / N;
-    end
-end
+ONE = ones(N,N) * 1/N;
+%for i=1:N
+%    for j=1:N
+%        ONE(i,j) = 1.0 / N;
+%    end
+%end
 
 KT = K - ONE*K - K*ONE + ONE*K*ONE;
 
@@ -42,9 +45,11 @@ KT = K - ONE*K - K*ONE + ONE*K*ONE;
 % in an Nxd matrix called 'vecs'
 
 %TODO
-[vecs vals] = eig(K);
-vecs = vecs(1:d,:)';
-vals = vals(1:d,:)';
+[vecs vals] = eigs(KT,d);
+%lambda = sum(vals, 1);
+%[lmbda_srtd lmbda_indcs] = sort(lambda, 'descend');
+%vals = vals(lmbda_indcs(1:d))';
+%vecs = vecs(lmbda_indcs(1:d),:)';
 
 % now project the data (Z will be the Nxd matrix of projections)
 % to d dimensions
