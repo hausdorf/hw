4615bb970755786f4f752183ecab290bad33c222
Alex Clemmer <clemmer.alexander@gmail.com>
2012-01-26 00:33:33 -0700

    Add 5955/hw01

---
+++ 5955/hw01/code/bday.py
@@ -0,0 +1,58 @@
+from __future__ import division, with_statement
+from matplotlib.pyplot import plot, show, axis, xlabel, ylabel
+import random
+
+
+def rand_gener(domain):
+	#random.seed(3)
+
+	while True:
+		yield random.randint(0,domain)
+
+def gener_until_collision(domain):
+	seen = set()
+
+	c = 0
+	for i in rand_gener(domain):
+		if i in seen:
+			return c
+		else:
+			seen.add(i)
+		c += 1
+
+def expt_n_times(n, domain):
+	return [gener_until_collision(domain) for i in range(n)]
+
+def iter_cnt(gen):
+	i = 1
+	for e in gen:
+		yield i, e
+		i += 1
+
+def find_gt_n(list, s, n):
+	for i,e in iter_cnt(list[s:]):
+		if e > n:
+			return i+s
+	return s
+
+
+if __name__ == '__main__':
+	times_repeat = 1000000
+	domain = 1000
+	collisions = expt_n_times(times_repeat, domain)
+
+	"""
+	print 'E[X]:', sum(collisions) / times_repeat
+
+	xs = [i for i in range(times_repeat)]
+
+	srtd_collisions = sorted(collisions)
+	l = len(srtd_collisions)
+	ys = [find_gt_n(srtd_collisions, c, e) / l
+			for c,e in iter_cnt(srtd_collisions)]
+
+	plot(srtd_collisions, ys)
+	xlabel('value of k')
+	ylabel('% of experiments succeeding after k trials')
+	show()
+	"""
---
+++ 5955/hw01/code/coupons.py
@@ -0,0 +1,78 @@
+from __future__ import division, with_statement
+from matplotlib.pyplot import plot, show, axis, xlabel, ylabel, hist, grid, title, text
+from collections import defaultdict
+import random
+
+
+def rand_gener(domain):
+	#random.seed(3)
+
+	while True:
+		yield random.randint(0,domain)
+
+def gener_until_full(domain):
+	seen = defaultdict(lambda:0)
+
+	c = 0
+	for i in rand_gener(domain):
+		if len(seen) == domain + 1:
+			return seen
+		else:
+			seen[i] += 1
+		c += 1
+
+def expt_n_times(n, domain):
+	def proc_list(d):
+		hst = []
+		for k,v in d.items():
+			hst += [k]*v
+		return hst
+
+	return [proc_list(gener_until_full(domain)) for i in range(n)]
+
+def iter_cnt(gen):
+	i = 1
+	for e in gen:
+		yield i, e
+		i += 1
+
+def find_gt_n(list, s, n):
+	for i,e in iter_cnt(list[s:]):
+		if e > n:
+			return i+s
+	return s
+
+
+if __name__ == '__main__':
+	times_repeat = 300
+	domain = 60
+	fulls = expt_n_times(times_repeat, domain)
+	ks = [len(e) for e in fulls]
+
+	#print fulls
+	print ks
+
+	print 'E[X]:', sum(ks) / times_repeat
+
+	srtd_ks = sorted(ks)
+	l = len(srtd_ks)
+	ys = [find_gt_n(srtd_ks, c, e) / l
+			for c,e in iter_cnt(srtd_ks)]
+
+	plot(srtd_ks, ys)
+	title('Cumulative density of required k trials', fontsize=20)
+	xlabel('value of k', fontsize=16)
+	ylabel('% of experiments succeeding after k trials', fontsize=16)
+	grid()
+	show()
+
+	"""
+	hist(fulls[0], domain+1, facecolor='g', alpha=0.75)
+	title('Number of times a random number has had value of i', fontsize=20)
+	xlabel('the value of i', fontsize=16)
+	ylabel('# of times random number has had value', fontsize=16)
+	grid()
+	show()
+	"""
+
+
