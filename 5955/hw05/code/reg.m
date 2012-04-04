69eba51dcf58dabaaf09590c5a0759ea48623a8b
Alex Clemmer <clemmer.alexander@gmail.com>
2012-04-04 01:38:52 -0600

    Update reg.m for 5955/A5

--- 5955/hw05/code/reg.m
+++ 5955/hw05/code/reg.m
@@ -1,5 +1,6 @@
 load M.dat
-[m,n] = size(M)
+[m,n] = size(M);
+
 
 % Question 1
 for k = 1:10
@@ -9,8 +10,10 @@ for k = 1:10
 	Vk = V(:,1:k);
 	Mk = Uk*Sk*Vk';
 
-	fprintf('k, norm: %d, %f\n', k, norm(M-Mk,2));
+	% Here is the answer
+	%fprintf('k, norm: %d, %f\n', k, norm(M-Mk,2));
 end
+
 
 % Question 2
 % find top t rows by L2 norm
@@ -31,7 +34,8 @@ for t = 1:30
 	end
 
 	P = C*inverse(C'*C)*C';
-	norm(M-P*M,2)
+	% Here is the answer
+	norm(M-P*M,2);
 end
 
 % find 
@@ -54,6 +58,63 @@ for t = 1:30
 	end
 
 	P = C*inverse(C'*C)*C';
-	norm(M-P*M,2)
+	% Here is the answer
+	norm(M-P*M,2);
 end
 
+
+% Question 3
+load X.dat
+load Y.dat
+s = [0.1, 0.3, 0.5, 1.0, 2.0];
+[_, d] = size(s);
+[xr, xd] = size(X);
+[yr, yd] = size(Y);
+% Part A
+A = inverse(X' * X)*X'*Y;
+
+% Here is the answer
+norm(Y-X*A,2);
+
+for i = 1:d
+	As = inverse(X' * X + s(i)*eye(6))*X'*Y;
+	% Here is the answer
+	norm(Y-X*As,2);
+end
+
+
+% PART B
+X1 = X(1:8, :);
+Y1 = Y(1:8);
+A = inverse(X1' * X1)*X1'*Y1;
+% Here is the answer
+norm(Y(9:10)-X(9:10, :)*A,2);
+for i = 1:d
+	As = inverse(X1' * X1 + s(i)*eye(6))*X1'*Y1;
+	% Here is the answer
+	norm(Y(9:10)-X(9:10, :)*As,2);
+end
+
+
+X2 = X(3:10, :);
+Y2 = Y(3:10);
+A = inverse(X2' * X2)*X2'*Y2;
+% Here is the answer
+norm(Y(1:2)-X(1:2, :)*A,2);
+for i = 1:d
+	As = inverse(X2' * X2 + s(i)*eye(6))*X2'*Y2;
+	% Here is the answer
+	norm(Y(1:2)-X(1:2, :)*As,2);
+end
+
+
+X3 = [X(1:4, :); X(7:10, :)];
+Y3 = [Y(1:4); Y(7:10)];
+A = inverse(X3' * X3)*X3'*Y3;
+% Here is the answer
+norm(Y(5:6)-X(5:6, :)*A,2);
+for i = 1:d
+	As = inverse(X3' * X3 + s(i)*eye(6))*X3'*Y3;
+	% Here is the answer
+	norm(Y(5:6)-X(5:6, :)*As,2);
+end
